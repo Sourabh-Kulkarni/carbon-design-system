17ed68044b16bb4533e393622d2c254a8113c435
Sourabh <sourabhkulkarni180@gmail.com>
2023-07-02 21:04:48 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.scss";
+import React, { Component } from "react";
+import "./app.scss";
+import { Button, Content } from "@carbon/react";
+import TutorialHeader from "./components/TutorialHeader";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./content/LandingPage";
+import RepoPage from "./content/RepoPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <TutorialHeader />
+        <Content>
+          <Routes>
+            <Route path="/" exact element={<LandingPage />} />
+            <Route path="/repo" element={<RepoPage />} />
+          </Routes>
+        </Content>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+// import "core-js/modules/es7.array.includes";
+// import "core-js/modules/es6.array.fill";
+// import "core-js/modules/es6.string.includes";
+// import "core-js/modules/es6.string.trim";
+// import "core-js/modules/es7.object.values";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "core-js/modules/es.array.includes";
+import "core-js/modules/es.array.fill";
+import "core-js/modules/es.string.includes";
+import "core-js/modules/es.string.trim";
+import "core-js/modules/es.object.values";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
